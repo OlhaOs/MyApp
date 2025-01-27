3e637a520e9e509f2bd84afd50c49a2db0662d8b
OlhaOs <olalaadka@gmail.com>
2025-01-27 12:53:41 +0200

    add keyboardDismiss

--- src/screens/CreatePostsScreen.js
+++ src/screens/CreatePostsScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Camera from '../../assets/icons/Camera';
 import Cart from '../../assets/icons/Cart';
 import Location from '../../assets/icons/Location';
@@ -10,26 +16,27 @@ import HomeIndicator from '../components/HomeIndicator';
 
 export default function CreatePostsScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.publicationContainer}>
-        <View style={styles.imageContainer}>
-          <View style={styles.cameraIconContainer}>
-            <Camera />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.publicationContainer}>
+          <View style={styles.imageContainer}>
+            <View style={styles.cameraIconContainer}>
+              <Camera />
+            </View>
           </View>
-        </View>
-        <Text style={styles.textSecondary}>Завантажте фото</Text>
-        <View style={styles.inputWrap}>
-          <View style={styles.inputContainer}>
-            <InputField outerStyles={styles.input} placeholder={'Назва...'} />
-          </View>
-          <View style={styles.inputContainer}>
-            <Location style={styles.iconLocation} />
-            <InputField
-              outerStyles={styles.input}
-              placeholder={'Місцевість...'}
-            />
+          <Text style={styles.textSecondary}>Завантажте фото</Text>
+          <View style={styles.inputWrap}>
+            <View style={styles.inputContainer}>
+              <InputField outerStyles={styles.input} placeholder={'Назва...'} />
+            </View>
+            <View style={styles.inputContainer}>
+              <Location style={styles.iconLocation} />
+              <InputField
+                outerStyles={styles.input}
+                placeholder={'Місцевість...'}
+              />
 
-            {/* <GooglePlacesAutocomplete
+              {/* <GooglePlacesAutocomplete
             placeholder='Місцевість...'
             minLength={4}
             enablePoweredByContainer={false}
@@ -70,21 +77,22 @@ export default function CreatePostsScreen() {
               },
             }}
           /> */}
+            </View>
           </View>
         </View>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <MainButton
-          textButton={'Опублікувати'}
-          outer={styles.outerButtton}
-          textOuter={styles.textButton}
-        />
-        <View style={styles.CartIconContainer}>
-          <Cart />
+        <View style={styles.buttonsContainer}>
+          <MainButton
+            textButton={'Опублікувати'}
+            outer={styles.outerButtton}
+            textOuter={styles.textButton}
+          />
+          <View style={styles.CartIconContainer}>
+            <Cart />
+          </View>
         </View>
+        <HomeIndicator />
       </View>
-      <HomeIndicator />
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
